678433d0bc49020eef8c2f33a8fbabda6e979488
chaoyenchou <choucha@sheridancollege.ca>
2023-01-23 16:07:18 -0500

    ICE 1 branch the real one version 1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,12 +1,15 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
- * this is a branch code
+ * This is a branch code
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022
+ * @author Chao Yen Chou Jan 23, 2023
  * Make a change
  */
 public class CardTrick {
@@ -17,12 +20,30 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            card.setValue((int)(Math.random()*13) + 1);
             //card.setValue(insert call to random number generator here)
             // 
+            card.setSuit(Card.SUITS[(int)(Math.random()*4)]);
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
             hand[i]=card;
+            System.out.println(card.getSuit() + " " + card.getValue());
+        }
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Guess the card value, 1-13");
+        int guessValue = scan.nextInt();
+        System.out.println("Guess the card suit, Hearts, Diamonds, Spades, or Clubs");
+        String guessSuit = scan.next();
+        
+        for (int i = 0; i < 7; i++){
+            if (guessValue == hand[i].getValue()){
+                if (guessSuit.equals(hand[i].getSuit())){
+                    printInfo();
+                }
+            }
         }
 
         // insert code to ask the user for Card value and suit, create their card
